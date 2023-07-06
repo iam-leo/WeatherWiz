ed7ca57dc5f3ef2e6acf459f1ad1c96e2a890726
Javier Leonel Sosa <leo.vm.cba@gmail.com>
2023-07-05 21:33:47 -0300

    add: API query service

--- src/app/services/clima.service.ts
+++ src/app/services/clima.service.ts
@@ -1,9 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.development';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClimaService {
+  apiKey = environment.API_KEY
+  
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+  getClima(ciudad: string): Observable<any>{
+    const URL = environment.URL_BASE + `q=${ciudad}&appid=${this.apiKey}&lang=es`
+    
+    return this.http.get(URL)
+  }
 }
